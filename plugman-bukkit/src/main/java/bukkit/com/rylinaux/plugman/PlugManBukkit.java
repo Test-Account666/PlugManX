8fd7077308a07ec06fec9e591273aabfd9664292
Test-Account666 <36412486+Test-Account666@users.noreply.github.com>
2025-08-07 19:49:10 +0200

    Remove unused methods

--- plugman-bukkit/src/main/java/bukkit/com/rylinaux/plugman/PlugManBukkit.java
+++ plugman-bukkit/src/main/java/bukkit/com/rylinaux/plugman/PlugManBukkit.java
@@ -40,11 +40,6 @@ import manifold.rt.api.NoBootstrap;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.ApiStatus;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
 /**
  * Plugin manager for Bukkit servers.
  *
@@ -67,54 +62,6 @@ public class PlugManBukkit extends JavaPlugin {
     };
     @Delegate
     private PlugManFileManager fileManager;
-
-    private static InputStream getResourceStatic(String filename) {
-        try {
-            var url = PlugManBukkit.class.getClassLoader().getResource(filename);
-            if (url == null) return null;
-            else {
-                var connection = url.openConnection();
-                connection.setUseCaches(false);
-                return connection.getInputStream();
-            }
-        } catch (IOException exception) {
-            return null;
-        }
-    }
-
-    public static void saveResourceStatic(String resourcePath, boolean replace) {
-        var dataFolder = new File("plugins");
-        if (resourcePath != null && !resourcePath.isEmpty()) {
-            resourcePath = resourcePath.replace('\\', '/');
-            var in = PlugManBukkit.getResourceStatic(resourcePath);
-            if (in == null) throw new IllegalArgumentException("The embedded resource '" + resourcePath + "' cannot be found in PlugManX");
-            else {
-                var outFile = new File(dataFolder, resourcePath);
-                var lastIndex = resourcePath.lastIndexOf(47);
-                var outDir = new File(dataFolder, resourcePath.substring(0, Math.max(lastIndex, 0)));
-                if (!outDir.exists()) outDir.mkdirs();
-
-                try {
-                    var out = new FileOutputStream(outFile);
-                    var buf = new byte[1024];
-
-                    int len;
-                    while ((len = in.read(buf)) > 0) out.write(buf, 0, len);
-
-                    out.close();
-                    in.close();
-                } catch (IOException exception) {
-                    if (instance != null) {
-                        instance.getLogger().severe("Could not save " + outFile.getName() + " to " + outFile + ": " + exception.getMessage());
-                    } else {
-                        // Fallback when instance is not available
-                        java.util.logging.Logger.getLogger("PlugManX").severe("Could not save " + outFile.getName() + " to " + outFile + ": " + exception.getMessage());
-                    }
-                }
-
-            }
-        } else throw new IllegalArgumentException("ResourcePath cannot be null or empty");
-    }
 
     @Deprecated(
             forRemoval = true,
