8973f3736dcb02dee48754133861274497c8a5ed
Test-Account666 <36412486+Test-Account666@users.noreply.github.com>
2025-09-04 13:56:38 +0200

    Update version number to '3.0.2'

--- plugman-velocity/src/main/java/velocity/com/rylinaux/plugman/PlugManVelocity.java
+++ plugman-velocity/src/main/java/velocity/com/rylinaux/plugman/PlugManVelocity.java
@@ -52,7 +52,7 @@ import java.nio.file.Path;
 @Plugin(
         id = "plugmanvelocity",
         name = "PlugManVelocity",
-        version = "3.0.2-RC.1",
+        version = "3.0.2",
         description = "Plugin manager for Velocity servers.",
         authors = {"rylinaux", "TestAccount666"}
 )
