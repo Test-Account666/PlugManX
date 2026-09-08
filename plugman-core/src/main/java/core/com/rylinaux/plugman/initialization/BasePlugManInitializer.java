3e0e5d50af0be936f9678cd0dbfb63b2fcbc813f
MrSimpleJS <54013002+MrSimpleJS@users.noreply.github.com>
2026-09-08 02:03:04 +0200

    Fix reflection cache cleanup during shutdown to prevent Velocity warning messages.

--- plugman-core/src/main/java/core/com/rylinaux/plugman/initialization/BasePlugManInitializer.java
+++ plugman-core/src/main/java/core/com/rylinaux/plugman/initialization/BasePlugManInitializer.java
@@ -34,6 +34,9 @@ import core.com.rylinaux.plugman.plugins.Plugin;
 import core.com.rylinaux.plugman.plugins.PluginManager;
 import core.com.rylinaux.plugman.services.ServiceRegistry;
 import core.com.rylinaux.plugman.util.ThreadUtil;
+import core.com.rylinaux.plugman.util.reflection.ClassAccessor;
+import core.com.rylinaux.plugman.util.reflection.FieldAccessor;
+import core.com.rylinaux.plugman.util.reflection.MethodAccessor;
 import lombok.Getter;
 
 import java.io.File;
@@ -116,20 +119,9 @@ public abstract class BasePlugManInitializer {
     }
 
     private void clearReflectionCaches() {
-        clearReflectionCache("class", "core.com.rylinaux.plugman.util.reflection.ClassAccessor");
-        clearReflectionCache("field", "core.com.rylinaux.plugman.util.reflection.FieldAccessor");
-        clearReflectionCache("method", "core.com.rylinaux.plugman.util.reflection.MethodAccessor");
-    }
-
-    private void clearReflectionCache(String cacheName, String accessorClassName) {
-        try {
-            var accessorClass = Class.forName(accessorClassName);
-            accessorClass.getMethod("clearCache").invoke(null);
-        } catch (LinkageError | RuntimeException exception) {
-            logger.warning("Failed to clear " + cacheName + " reflection cache during shutdown: " + exception.getMessage());
-        } catch (ReflectiveOperationException exception) {
-            logger.warning("Failed to clear " + cacheName + " reflection cache during shutdown: " + exception.getMessage());
-        }
+        ClassAccessor.clearCache();
+        FieldAccessor.clearCache();
+        MethodAccessor.clearCache();
     }
 
     /**
